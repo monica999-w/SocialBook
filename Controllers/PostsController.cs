1a8468532bb98411c1adcc8834fcfd98f111ec21
monicaa22 <iliescu.monica.e3m@student.ucv.ro>
2022-05-24 22:33:01 +0300

    Add front-end for post

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -164,5 +164,8 @@ namespace SocialBook
             _postRepository.Delete(post);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public async Task<IActionResult> Post() => View(await _postRepository.GetList());
     }
 }
